499c6cffefd7d13e7fff73d90cf74f7cd8b2335f
Anton Podavalov <a.podavalov@gmail.com>
2017-08-17 22:32:27 +0300

    Added tests.

---
+++ Application/Core.Test/AppTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace MeteringDevices.Core.Test
+{
+    public class AppTest
+    {
+        [Test]
+        public void RunTest()
+        {
+            //given
+            Mock<IDataProvider>[] mockDataProviders = 
+                Enumerable.Repeat<Func<Mock<IDataProvider>>>(() => new Mock<IDataProvider>(), 3).Select(f => f()).ToArray();
+
+            foreach (Mock<IDataProvider> mockDataProvider in mockDataProviders)
+            {
+                mockDataProvider.Setup(mock => mock.Provide()).Verifiable();
+            }
+
+            //when
+            App app = new App(mockDataProviders.Select(mockDataProvider => mockDataProvider.Object).ToArray());
+            app.Run();
+
+            //then
+            foreach (Mock<IDataProvider> mockDataProvider in mockDataProviders)
+            {
+                mockDataProvider.Verify(mock => mock.Provide());
+            }
+        }
+    }
+}
--- Application/Core.Test/Properties/AssemblyInfo.cs
+++ Application/Core.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MeteringDevices.Core")]
+[assembly: AssemblyTitle("Core.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MeteringDevices")]
+[assembly: AssemblyProduct("Core.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e11bb179-ce01-4614-bdbf-7da3ba57df6f")]
+[assembly: Guid("9469bb7b-7738-4649-9aa9-0e763e14814a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("MeteringDevices.Core.Test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
