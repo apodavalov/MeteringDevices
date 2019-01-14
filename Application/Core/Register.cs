44c834ad9087efbe323646bd29e5c4706196568d
Anton Podavalov <a.podavalov@gmail.com>
2019-01-14 18:01:59 +0300

    Removed Kzn support.

--- Application/Core/Register.cs
+++ Application/Core/Register.cs
@@ -19,18 +19,6 @@ namespace MeteringDevices.Core
         private const string _MailSenderToAddress = "MailSender.ToAddress";
         private const string _MailSenderSubject = "MailSender.Subject";
 
-        private const string _KznUsernameKey = "Kzn.Service.Username";
-        private const string _KznPasswordKey = "Kzn.Service.Password";
-        private const string _KznBaseUrlKey = "Kzn.Service.BaseUrl";
-
-        private const string _KznAccountNumberKey = "Kzn.AccountNumber";
-        private const string _KznEnabledKey = "Kzn.Enabled";
-        private const string _KznDayOfMonthToSendKey = "Kzn.DayOfMonthToSend";
-        private const string _KznMeteringDeviceDayIdKey = "Kzn.MeteringDevice.DayId";
-        private const string _KznMeteringDeviceNightIdKey = "Kzn.MeteringDevice.NightId";
-        private const string _KznMeteringDeviceColdIdKey = "Kzn.MeteringDevice.ColdId";
-        private const string _KznMeteringDeviceHotIdKey = "Kzn.MeteringDevice.HotId";
-
         private const string _SpbAccountNumberKey = "Spb.AccountNumber";
         private const string _SpbEnabledKey = "Spb.Enabled";
         private const string _SpbDayOfMonthToSendKey = "Spb.DayOfMonthToSend";
@@ -43,7 +31,6 @@ namespace MeteringDevices.Core
 
         public override void Load()
         {
-            Kernel.Bind<Kzn.ISendApiService>().ToMethod(CreateKznSendApiService).InSingletonScope();
             Kernel.Bind<IJsonSerializerFactory>().To<JsonSerializerFactory>().InSingletonScope();
             Kernel.Bind<IRestSharpFactory>().To<RestSharpFactory>().InSingletonScope();
             Kernel.Bind<INotifier>().ToMethod(CreateNotifier).InSingletonScope();
@@ -52,33 +39,9 @@ namespace MeteringDevices.Core
             Kernel.Bind<IApp>().ToMethod(CreateApp).InSingletonScope();
         }
 
-        private Kzn.ISendApiService CreateKznSendApiService(IContext arg)
-        {
-            return new Kzn.SendApiService(arg.Kernel.Get<IRestSharpFactory>(), ConfigUtils.GetStringFromConfig(_KznBaseUrlKey));
-        }
-
         private IApp CreateApp(IContext arg)
         {
             return new App(
-                new DataProvider(
-                    arg.Kernel.Get<ISessionFactory>(),
-                    arg.Kernel.Get<INotifier>(),
-                    new Kzn.RetrieverService(
-                        ConfigUtils.GetStringFromConfig(_KznMeteringDeviceDayIdKey),
-                        ConfigUtils.GetStringFromConfig(_KznMeteringDeviceNightIdKey),
-                        ConfigUtils.GetStringFromConfig(_KznMeteringDeviceColdIdKey),
-                        ConfigUtils.GetStringFromConfig(_KznMeteringDeviceHotIdKey)
-                    ),
-                    new Kzn.SendService(
-                        arg.Kernel.Get<Kzn.ISendApiService>(),
-                        ConfigUtils.GetStringFromConfig(_KznUsernameKey),
-                        ConfigUtils.GetStringFromConfig(_KznPasswordKey)
-                    ),
-                    arg.Kernel.Get<ICurrentDateTimeProvider>(),
-                    ConfigUtils.GetBoolFromConfig(_KznEnabledKey),
-                    ConfigUtils.GetStringFromConfig(_KznAccountNumberKey),
-                    ConfigUtils.GetIntFromConfig(_KznDayOfMonthToSendKey)
-                ),
                 new DataProvider(
                     arg.Kernel.Get<ISessionFactory>(),
                     arg.Kernel.Get<INotifier>(),
